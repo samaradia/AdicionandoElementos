cd4136390a475e37d59d10192fca1c7c87a6b705
Samara Dias da Silva <marcoscz329@gmail.com>
2026-06-26 16:11:09 -0300

    praticando

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> funcionarios = new ArrayList<>();
+        funcionarios.add("João");
+        funcionarios.add("lilika");
+        funcionarios.add("Patrícia");
+        funcionarios.add("Will");
+
+        System.out.println("Lista de funcionários: " + funcionarios);
+
+    }
+}
